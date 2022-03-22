94fde02b0416a8c1baa77744c1101ee29c6a2bab
Bruno-M44 <bruno.maguin@posteo.net>
2022-03-22 14:10:58 +0100

    pushing to heroku

--- cv_app/main.py
+++ cv_app/main.py
@@ -1,20 +1,12 @@
-from flask import Flask
- 
+from flask import Flask, render_template
+
 app = Flask(__name__)
- 
-@app.route("/")
-def home_view():
-        return "<h1>Welcome to Geeks for Geeks</h1>"
+#app.secret_key = 'something_special'
+#Scss(app, static_dir='static', asset_dir='assets')
 
-# from flask import Flask, render_template
-
-# app = Flask(__name__)
-# app.secret_key = 'something_special'
-# #Scss(app, static_dir='static', asset_dir='assets')
-
-# @app.route('/')
-# def index():
-#     return render_template('cv.html')
+@app.route('/')
+def index():
+    return render_template('cv.html')
 
 # if __name__ == '__main__':
 #     # Threaded option to enable multiple instances for multiple user access support
