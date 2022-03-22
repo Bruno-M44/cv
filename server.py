c17f361a7d57030e4f546d41315e22fcc5029d66
Bruno-M44 <bruno.maguin@posteo.net>
2022-03-22 13:00:25 +0100

    pushing to heroku

--- server.py
+++ server.py
@@ -7,3 +7,7 @@ app.secret_key = 'something_special'
 @app.route('/')
 def index():
     return render_template('cv.html')
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
