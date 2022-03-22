3c0286afbbef952071cbe8d508599f317c7f0701
Bruno-M44 <bruno.maguin@posteo.net>
2022-03-22 14:36:32 +0100

    pushing to heroku

--- cv_app/main.py
+++ cv_app/main.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template
+import sys
+import logging
 
 app = Flask(__name__)
 #app.secret_key = 'something_special'
 #Scss(app, static_dir='static', asset_dir='assets')
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 @app.route('/')
 def index():
